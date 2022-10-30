d83115f4c179574549dbc244ccd43017a54686e0
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-10-30 20:56:50 +0600

    I think the code is working, But i don't know how it's works

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,63 +1,88 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Register = () => {
-   const navigate = useNavigate()
-   const navigateLogin = () => {
-      navigate('/login')
-   }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const handleRegister = event => {
-      event.preventDefault()
-      const name = event.target.name.value
-      const email = event.target.email.value
-      const password = event.target.password.value
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
 
-   }
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    navigate("/login");
+  };
 
-   return (
-     <div>
-       <h2>Please Register.</h2>
-       <Form onSubmit={handleRegister} className="container w-50 mx-auto">
-         <Form.Group className="mb-3">
-           <Form.Label>Your Name</Form.Label>
-           <Form.Control type="text" placeholder="Enter Your Name" required />
-           <Form.Text className="text-muted">
-             We'll never share your name with anyone else.
-           </Form.Text>
-         </Form.Group>
-         <Form.Group className="mb-3" controlId="formBasicEmail">
-           <Form.Label>Email address</Form.Label>
-           <Form.Control type="email" placeholder="Enter email" required />
-           <Form.Text className="text-muted">
-             We'll never share your email with anyone else.
-           </Form.Text>
-         </Form.Group>
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(email, password);
+  };
+  return (
+    <div>
+      <h2>Please Register.</h2>
+      <Form className="container w-50 mx-auto">
+        <Form.Group className="mb-3">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Your Name" required />
+          <Form.Text className="text-muted">
+            We'll never share your name with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-         <Form.Group className="mb-3" controlId="formBasicPassword">
-           <Form.Label>Password</Form.Label>
-           <Form.Control type="password" placeholder="Password" required />
-         </Form.Group>
-         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-           <Form.Check type="checkbox" label="Check me out" />
-         </Form.Group>
-         <Button variant="primary" type="submit">
-           Submit
-         </Button>
-       </Form>
-       <p>
-         Already have an account ?{" "}
-         <Link
-           to="/login"
-           className="text-danger pointer-event "
-           onClick={navigateLogin}
-         >
-           Please Login
-         </Link>
-       </p>
-     </div>
-   );
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button
+          onClick={() => createUserWithEmailAndPassword(email, password)}
+          variant="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form>
+      <p>
+        Already have an account ?{" "}
+        <Link
+          to="/login"
+          className="text-danger pointer-event "
+          onClick={navigateLogin}
+        >
+          Please Login
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
+
